72e5c1a068b2ae0f3245d6d5d5d040d59bbbe074
Andrey Semashev <andrey.semashev@gmail.com>
2014-11-09 21:05:21 +0300

    Added a test script.

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+INPUT_FILES="44kHz_16-bit.wav 44kHz_32-bit_float.wav 48kHz_16-bit.wav 48kHz_32-bit_float.wav 96kHz_32-bit_float.wav"
+
+OUTPUT_RATES="44100 48000 96000"
+RESAMPLERS="speex-1 speex-5 speex-10 soxr-lq soxr-mq soxr-hq soxr-vhq"
+
+for resampler in $RESAMPLERS
+do
+	mkdir -p $resampler
+
+	for rate in $OUTPUT_RATES
+	do
+		for file in $INPUT_FILES
+		do
+			out_file=`basename $file .wav`_to_${rate}.wav
+			time ./src_test $file $resampler $rate $resampler/$out_file
+			echo ""
+		done
+	done
+done
+
