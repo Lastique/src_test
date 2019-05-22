fd1cd5447b2356ec3ace055896c5a5c375142cb9
Miles B Huff <MilesBHuff@users.noreply.github.com>
2019-05-22 10:58:38 -0400

    Refactored

--- test.sh
+++ test.sh
@@ -1,22 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-INPUT_FILES="44kHz_16-bit.wav 44kHz_32-bit_float.wav 48kHz_16-bit.wav 48kHz_32-bit_float.wav 96kHz_32-bit_float.wav"
+declare -a INPUT_FILES=('44kHz_16-bit.wav' '44kHz_32-bit_float.wav' '48kHz_16-bit.wav' '48kHz_32-bit_float.wav' '96kHz_32-bit_float.wav')
+declare -a OUTPUT_RATES=(44100 48000 96000)
+declare -a RESAMPLERS=('speex-1' 'speex-5' 'speex-10' 'soxr-lq' 'soxr-mq' 'soxr-hq' 'soxr-vhq')
 
-OUTPUT_RATES="44100 48000 96000"
-RESAMPLERS="speex-1 speex-5 speex-10 soxr-lq soxr-mq soxr-hq soxr-vhq"
+for RESAMPLER in ${RESAMPLERS[@]}; do
+	mkdir -p $RESAMPLER
 
-for resampler in $RESAMPLERS
-do
-	mkdir -p $resampler
+	for OUT_RATE in ${OUTPUT_RATES[@]}; do
+		for IN_FILE in ${INPUT_FILES[@]}; do
 
-	for rate in $OUTPUT_RATES
-	do
-		for file in $INPUT_FILES
-		do
-			out_file=`basename $file .wav`_to_${rate}
-			/usr/bin/time --format "real/user/sys:\t%e/%U/%S" ./src_test $file $resampler $rate $resampler/$out_file.wav
-			/usr/bin/sox $out_file.wav -n spectrogram -o $out_file.png
-			echo ""
+			OUT_FILE="$(basename $IN_FILE .wav)_to_$OUT_RATE"
+			/usr/bin/env time --format "real/user/sys:\t%e/%U/%S" ./src_test "$IN_FILE" "$RESAMPLER" "$OUT_RATE" "$RESAMPLER/$OUT_FILE.wav"
+			[[ $(which sox) = *:* ]] && sox "$OUTFILE.wav" -n spectrogram -o "$OUTFILE.png"
+			echo
 		done
 	done
 done
