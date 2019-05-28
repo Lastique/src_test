80caee522a8373c2d18de191b4845851f86597fe
Andrey Semashev <andrey.semashev@gmail.com>
2019-05-29 00:17:46 +0300

    Renamed the script file, minor stylistic corrections.

--- src_test.sh
+++ src_test.sh
@@ -4,16 +4,27 @@ declare -a INPUT_FILES=('44kHz_16-bit.wav' '44kHz_32-bit_float.wav' '48kHz_16-bi
 declare -a OUTPUT_RATES=(44100 48000 96000)
 declare -a RESAMPLERS=('speex-1' 'speex-5' 'speex-10' 'soxr-lq' 'soxr-mq' 'soxr-hq' 'soxr-vhq')
 
-for RESAMPLER in ${RESAMPLERS[@]}; do
-	mkdir -p $RESAMPLER
+HAS_SOX=0
+if type sox >/dev/null 2>&1
+then
+	HAS_SOX=1
+fi
 
-	for OUT_RATE in ${OUTPUT_RATES[@]}; do
-		for IN_FILE in ${INPUT_FILES[@]}; do
+for RESAMPLER in ${RESAMPLERS[@]}
+do
+	mkdir -p "$RESAMPLER"
 
+	for OUT_RATE in ${OUTPUT_RATES[@]}
+	do
+		for IN_FILE in ${INPUT_FILES[@]}
+		do
 			OUT_FILE="$RESAMPLER/$(basename $IN_FILE .wav)_to_$OUT_RATE"
 			/usr/bin/env time --format "real/user/sys:\t%e/%U/%S" ./src_test "$IN_FILE" "$RESAMPLER" "$OUT_RATE" "$OUT_FILE.wav"
-			[[ -f /usr/bin/sox ]] && sox "$OUT_FILE.wav" -n spectrogram -o "$OUT_FILE.png"
-			echo
+			if [ $HAS_SOX -ne 0 ]
+			then
+				sox "$OUT_FILE.wav" -n spectrogram -o "$OUT_FILE.png"
+			fi
+			echo ""
 		done
 	done
 done
